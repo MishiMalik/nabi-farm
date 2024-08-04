6609d37747dd77ff92d8b5580622e58621d52930
Mehwish <mehwishgul715@gmail.com>
2024-08-04 17:06:21 +0500

    commit

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-// external js: isotope.pkgd.js
+// external js: isotope.pkgd.js, imagesloaded.pkgd.js
 
-// init Isotope elements
-var $box = $(".isotope-box").isotope({
-  itemSelector: ".isotope-item",
-});
-// filter functions
-// bind filter button click
-$(".isotope-toolbar").on("click", "button", function () {
-  var filterValue = $(this).attr("data-type");
-  $(".isotope-toolbar-btn").removeClass("active");
-  $(this).addClass("active");
-  if (filterValue !== "*") {
-    filterValue = '[data-type="' + filterValue + '"]';
-  }
-  console.log(filterValue);
-  $box.isotope({ filter: filterValue });
-});
+// init Isotope elements after images have loaded
+imagesLoaded(".isotope-box", function() {
+  var $box = $(".isotope-box").isotope({
+    itemSelector: ".isotope-item",
+  });
 
-//  
+  // filter functions
+  // bind filter button click
+  $(".isotope-toolbar").on("click", "button", function () {
+    var filterValue = $(this).attr("data-type");
+    $(".isotope-toolbar-btn").removeClass("active");
+    $(this).addClass("active");
+    if (filterValue !== "*") {
+      filterValue = '[data-type="' + filterValue + '"]';
+    }
+    console.log(filterValue);
+    $box.isotope({ filter: filterValue });
+  });
+});
